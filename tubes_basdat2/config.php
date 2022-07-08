ffdaac8c6f98f44c19eef23ef4748e2c4fb04dd6
R34Hunter <77817666+R34Hunter@users.noreply.github.com>
2022-07-08 19:08:54 +0700

    Add files via upload

---
+++ tubes_basdat2/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+if(!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
+
+define('DEFAULT_CONTROLLER', 'C_Auth');
+define('BASE_URL', 'http://localhost/tubes_basdat2/');
+define('APP_NAME', 'Omoteos Rental Mobil');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tubes_basdat2');
+
+?>
---
+++ tubes_basdat2/views/_partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+	    <div class="copyright text-center my-auto">
+	    <span>
+	    <a href="https://github.com/nugrohoff" target="_blank">Irham Soetomo Handayanto</a> &copy; Omoteos's Car Rent <?= date('Y') ?></span>
+	    </div>
+	</div>
+</footer>
